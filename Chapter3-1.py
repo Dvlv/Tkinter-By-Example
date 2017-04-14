7d5218595d9f418b30af53c802ecc6f979df79cb
Dvlv <dvd292@googlemail.com>
2017-04-14 16:23:16 +0100

    english and italian tabs

--- Chapter3-1.py
+++ Chapter3-1.py
@@ -1,13 +1,60 @@
 import tkinter as tk
-from tkinter.ttk import Notebook as nbk
+from tkinter.ttk import Notebook
 import requests
 
 class TranslateBook(tk.Tk):
-    def __init__():
+    def __init__(self):
         super().__init__()
 
-    def translate(self, target_language, text):
-        url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}".format('en',target_language, text)
-        r = requests.get(url2)
+        self.title('Translation Book v1')
+        self.geometry('500x300')
+
+        self.notebook = Notebook(self)
+
+        english_tab = tk.Frame(self.notebook)
+        italian_tab = tk.Frame(self.notebook)
+
+        self.translate_button = tk.Button(english_tab, text="translate", command=self.translate)
+        self.translate_button.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.english_entry = tk.Text(english_tab, bg="white", fg="black")
+        self.english_entry.pack(side=tk.TOP, expand=1)
+
+        self.italian_copy_button = tk.Button(italian_tab, text="Copy to Clipboard", command=self.copy_to_clipboard)
+        self.italian_copy_button.pack(side=tk.BOTTOM, fill=tk.X)
+
+        self.italian_translation = tk.StringVar(italian_tab)
+        self.italian_translation.set('')
+
+        self.italian_label = tk.Label(italian_tab, textvar=self.italian_translation, bg="lightgrey", fg="black")
+        self.italian_label.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+
+        self.notebook.add(english_tab, text="English")
+        self.notebook.add(italian_tab, text="Italian")
+
+        self.notebook.pack(fill=tk.BOTH, expand=1)
+
+    def translate(self, target_language=None, text=None):
+        if not target_language:
+            target_language = 'it'
+
+        if not text:
+            text = self.english_entry.get(1.0, tk.END)
+
+        url = "https://translate.googleapis.com/translate_a/single?client=gtx&sl={}&tl={}&dt=t&q={}".format('en', target_language, text)
+
+        r = requests.get(url)
         r.raise_for_status()
-        return r.json()
+        translation = r.json()[0][0][0]
+        self.italian_translation.set(translation)
+
+    def copy_to_clipboard(self, text=None):
+        if not text:
+            text = self.italian_translation.get()
+            self.clipboard_clear()
+            self.clipboard_append(text)
+
+
+if __name__ == "__main__":
+    translatebook = TranslateBook()
+    translatebook.mainloop()
