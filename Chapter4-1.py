6ca6b1d473968badc47a49898ba66a8018e2d8ab
Dvlv <dvd292@googlemail.com>
2017-04-20 19:18:24 +0100

    images for buttons ch4

--- Chapter4-1.py
+++ Chapter4-1.py
@@ -10,6 +10,9 @@ class Game(tk.Tk):
         self.minsize(width=800, height=640)
         self.maxsize(width=800, height=640)
 
+        self.key_image = tk.PhotoImage(file='assets/key.png')
+        self.question_mark_image = tk.PhotoImage(file='assets/questionmark.png')
+
         self.screen = tk.Canvas(self, bg="white", width=500, height=800)
         self.right_frame = tk.Frame(self, background="red", width=300, height=800)
         self.right_frame.pack_propagate(0)
@@ -18,25 +21,20 @@ class Game(tk.Tk):
         self.help_var.set("Try Clicking Something")
         self.help_box = tk.Label(self.right_frame, textvar=self.help_var, background="black", foreground="white", padx=10, pady=20)
 
-        self.inventory_title = tk.Label(self.right_frame, text="Inventory:")
+        self.inventory_title = tk.Label(self.right_frame, text="Inventory:", background="grey", foreground="white")
 
         self.inventory_space = tk.Frame(self.right_frame, bg="blue", width=300, height=320)
         self.inventory_space.pack_propagate(0)
 
-        self.inventory_slot_1 = ttk.Button(self.inventory_space)
-        self.inventory_slot_2 = ttk.Button(self.inventory_space)
-        self.inventory_slot_3 = ttk.Button(self.inventory_space)
+        self.inventory_slot_1 = tk.Button(self.inventory_space, image=self.question_mark_image, width=50, height=50)
+        self.inventory_slot_2 = tk.Button(self.inventory_space, image=self.question_mark_image, width=50, height=50)
+        self.inventory_slot_3 = tk.Button(self.inventory_space, image=self.question_mark_image, width=50, height=50)
 
-        # key image
-        #self.key_image = tk.PhotoImage('assets/key.png')
-
-        # question mark image
-        #self.question_mark_image = tk.PhotoImage('assets/questionmark.png')
         self.help_box.pack(side=tk.TOP, fill=tk.X, padx=10, pady=10)
 
-        self.inventory_slot_1.pack(fill=tk.X, pady=(60,30), padx=20)
-        self.inventory_slot_2.pack(fill=tk.X, pady=30, padx=20)
-        self.inventory_slot_3.pack(fill=tk.X, pady=(30,30), padx=20)
+        self.inventory_slot_1.pack(pady=(40,20), padx=20)
+        self.inventory_slot_2.pack(pady=20, padx=20)
+        self.inventory_slot_3.pack(pady=(20,0), padx=20)
 
         self.inventory_space.pack(side=tk.BOTTOM)
 
